e734a8b204e83cc005dc1e12e1cd8c04afa961ac
Tu Nombre <you@example.com>
2024-11-11 09:49:50 -0300

    ENANO

---
+++ src/repos/AuthRepo.ts
@@ -0,0 +1,21 @@
+import {Usuario, IUsuario} from '../models/Usuario';
+import bcrypt from 'bcrypt';
+import {generateToken} from '../util/jwt';
+
+async function login(email: string, password: string): Promise<string>{
+    const user = await Usuario.findOne({ 
+        where: { email: email }
+    });
+    const equals = await bcrypt.compare(password, user.contrasenia);
+
+
+    if (equals) {
+        return generateToken(user.idUsuario);
+    } else {
+        throw new Error('Invalid credentials');
+    }
+}
+
+export default {
+    login
+} as const;
