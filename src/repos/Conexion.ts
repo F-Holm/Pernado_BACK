b7bf11faf492a7d23e6444675f9b71569af2e4f9
Tu Nombre <you@example.com>
2024-11-28 13:43:32 -0300

    fix

--- src/repos/Conexion.ts
+++ src/repos/Conexion.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import schemas from './schemas';
 
-mongoose.connect(process.env.DATABASE as string);
+mongoose.connect(/*process.env.DATABASE as string*/'mongodb://localhost:27017/Pernado');
 
 export const Usuario = mongoose.model('Usuario', schemas.usuarioSchema);
 export const Chat = mongoose.model('Chat', schemas.chatSchema);
